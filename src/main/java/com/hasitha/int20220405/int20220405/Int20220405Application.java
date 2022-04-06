56b31f380b8568ea91fdb527a4d2154d3e2e99d5
Hasitha Mapalagama <dev.hasitha@gmail.com>
2022-04-06 17:21:12 +0530

    init

---
+++ src/main/java/com/hasitha/int20220405/int20220405/Int20220405Application.java
@@ -0,0 +1,13 @@
+package com.hasitha.int20220405.int20220405;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int20220405Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Int20220405Application.class, args);
+	}
+
+}
